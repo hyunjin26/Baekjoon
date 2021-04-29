56bbc968aed01c7530c0308c0c6adb510e33aab2
hyunjin26 <80029148+hyunjin26@users.noreply.github.com>
2021-04-29 14:35:05 +0900

    Create 2442.cpp

---
+++ 2442.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+
+	int n = 0, i = 0, j = 0;
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		for (int k = 1; k<= n-i; k++)
+		{
+			printf(" ");
+		 
+		}
+		for (j = 1; j <= i*2-1; j++)
+		{
+			printf("*");
+		}
+
+		printf("\n");
+	}	
+
+	return 0;
+}
